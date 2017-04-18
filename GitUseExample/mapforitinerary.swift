0a1706b2360ed8ceda6af1d3c175e03bffe1e695
liwangwnag123 <2317790024@qq.com>
2017-04-18 17:07:04 +0800

    新建文件

---
+++ GitUseExample/mapforitinerary.swift
@@ -0,0 +1,13 @@
+//
+//  mapforitinerary.swift
+//  GitUseExample
+//
+//  Created by 王力 on 2017/4/18.
+//  Copyright © 2017年 王力. All rights reserved.
+//
+
+import UIKit
+
+class mapforitinerary: NSObject {
+
+}
